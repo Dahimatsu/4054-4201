45eec8e6194040a1f23c9df72f7ac0b2cf76614f
Tony Mahefa <rtonymahefa@gmail.com>
2025-12-16 20:44:25 +0300

    Chauffeur model & controller

--- app/models/ChauffeurModel.php
+++ app/models/ChauffeurModel.php
@@ -5,16 +5,93 @@ namespace App\Models;
 use PDOException;
 
 class ChauffeurModel {
+
     private $database;
-    private $id_chauffeur;
+    private $id;
     private $nom;
     private $prenom;
 
     public function __construct($database) {
+        $this->setDatabase($database);
+    }
+
+    public function getDatabase()
+    {
+        return $this->database;
+    }
+
+    public function setDatabase($database)
+    {
         $this->database = $database;
     }
 
-    
+    public function getId()
+    {
+        return $this->id;
+    }
 
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function getChauffeurs()
+    {
+        $DBH = $this->getDatabase();
+
+        $query = "SELECT * 
+                  FROM chauffeur";
+
+        try {
+            $STH = $DBH->prepare($query);
+            $STH->execute();
+
+            return $STH->fetchAll();
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            throw $e;
+        }
+
+        return null;
+    }
+
+    public function getChauffeur($id)
+    {
+        $DBH = $this->getDatabase();
+
+        $query = "SELECT * 
+                  FROM chauffeur 
+                  WHERE id_chauffeur = ?";
+
+        try {
+            $STH = $DBH->prepare($query);
+            $STH->bindParam(1,$id);
+            $STH->execute();
+
+            return $STH->fetch();
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            throw $e;
+        }
+    }
 }
