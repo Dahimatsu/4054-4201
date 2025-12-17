6b89f23e35b89cf2a6155f9d9044b642756d660e
BrandyAllan <brandyratsimbaharison46@gmail.com>
2025-12-17 12:03:03 +0300

    fix views path

--- app/config/config.php
+++ app/config/config.php
@@ -37,7 +37,7 @@ $app->set('flight.base_url', '/', );           // Base URL for your app. Change 
 $app->set('flight.case_sensitive', false);    // Set true for case sensitive routes. Default: false
 $app->set('flight.log_errors', true);         // Log errors to file. Recommended: true in production
 $app->set('flight.handle_errors', false);     // Let Tracy handle errors if false. Set true to use Flight's error handler
-$app->set('flight.views.path', __DIR__ . $ds . '..' . $ds . '..' . $ds . 'views'); // Path to views/templates
+$app->set('flight.views.path', __DIR__ . $ds . '..' . $ds . 'views'); // Path to views/templates
 $app->set('flight.views.extension', '.php');  // View file extension (e.g., '.php', '.latte')
 $app->set('flight.content_length', false);    // Send content length header. Usually false unless required by proxy
 
