09c3ca6b625967775899bf5d8718c5662d7bb262
Tony Mahefa <rtonymahefa@gmail.com>
2025-12-17 14:27:57 +0300

    Modification route course

--- app/config/routes.php
+++ app/config/routes.php
@@ -21,34 +21,36 @@ $router->group('', function (Router $router) use ($app) {
         $app->render('layout', ['page' => "accueil.php"]);
     });
 
-    $router->get('/planning-moto', function () use ($app) {
-        $conducteurController = new ConducteurController($app);
-        $conducteurs = $conducteurController->getConducteurs();
+    $router->group('/course',function () use ($router, $app) {
+        $router->get('/',function () use ($app) {
+            $app->render('layout', ['page' => "course.php"]);
+        });
 
-        $motoController = new MotoController($app);
-        $motos = $motoController->getMotos();
+        $router->get('/planning', function () use ($app) {
+            $conducteurController = new ConducteurController($app);
+            $conducteurs = $conducteurController->getConducteurs();
 
-        $courseController = new CourseController($app);
-        $plannings = $courseController->getPlannings();
-        $app->render('layout', ['page' => "planning.php",'conducteurs' => $conducteurs, 'motos' => $motos, 'plannings' => $plannings]);
-    });
+            $motoController = new MotoController($app);
+            $motos = $motoController->getMotos();
 
-    $router->get('/liste',function () use ($app) {
-        $courseController = new CourseController($app);
-        $courses = $courseController->getCourses();
-        
-        $app->render('layout', ['page' => "liste.php", 'courses' => $courses]);
-    });
+            $courseController = new CourseController($app);
+            $plannings = $courseController->getPlannings();
+            $app->render('layout', ['page' => "planning.php", 'conducteurs' => $conducteurs, 'motos' => $motos, 'plannings' => $plannings]);
+        });
 
-    $router->get('/course',function () use ($app) {
-        $app->render('layout', ['page' => "course.php"]);
-    });
+        $router->get('/nouvelle',function () use ($app) {
+            $conducteurController = new ConducteurController($app);
+            $conducteurs = $conducteurController->getConducteurs();
+    
+            $app->render('layout', ['page' => "inserer-course.php", 'conducteurs' => $conducteurs]);
+        });
 
-    $router->get('/nouvelle-course',function () use ($app) {
-        $conducteurController = new ConducteurController($app);
-        $conducteurs = $conducteurController->getConducteurs();
+        $router->get('/liste', function () use ($app) {
+            $courseController = new CourseController($app);
+            $courses = $courseController->getCourses();
 
-        $app->render('layout', ['page' => "inserer-course.php", 'conducteurs' => $conducteurs]);
+            $app->render('layout', ['page' => "liste.php", 'courses' => $courses]);
+        });
     });
 
     $router->post('/inserer-course',function () use ($app) {
